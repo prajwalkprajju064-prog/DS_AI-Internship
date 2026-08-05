a4a6cda3627f26193a2687a87a44683f68c4e324
prajwalkprajju064-prog <prajwalkprajju064@gmail.com>
2026-08-05 20:11:02 +0530

    commit

---
+++ day2/cart3.py
@@ -0,0 +1,5 @@
+# Shopping cart as a dictionary
+shopping_cart = {"apple": 2, "banana": 3, "milk": 1}
+
+# Count total items (sum of quantities)
+print("Total items in cart:", sum(shopping_cart.values()))
