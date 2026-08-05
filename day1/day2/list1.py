733c646209ea21cf61f52aa0852f269dd1ce2563
prajwalkprajju064-prog <prajwalkprajju064@gmail.com>
2026-08-05 20:08:25 +0530

    Revert "2ND COMMIT"
    This reverts commit 868537461c99fd193107c3abbb3b823a7e7611c7.

--- day1/day2/list1.py
+++
@@ -1,5 +0,0 @@
-student =[["name","akash"],["age","23"],[["marks"],[23,44,55]]]
-
-print(student[0])
-print(student[1][1])
-print()
--- day1/program4.py
+++ day1/program4.py
@@ -1,15 +1,2 @@
-a = int(input("enter number a "))
-operator =input("enter operator(+,-,*,/) ")
-b = (input("enter number  "))
-
-if operator == "+":
-    print("result =",a+b) 
-elif operator == "-":
-     print("result =",a-b)
-elif operator =="*":
-     print("result =",a*b)
-elif operator =="/":
-     print("result =",a/b)
-else:
-    print("invalid number")
-    
+def is_palindrome(s):
+s = s.replace(" "," ")
