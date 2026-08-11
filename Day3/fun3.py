f4328c0819f36048a225d0ba04277f81f888109d
prajwalkprajju064-prog <prajwalkprajju064@gmail.com>
2026-08-11 12:40:43 +0530

    day3

--- Day3/fun3.py
+++ Day3/fun3.py
@@ -1,7 +1,6 @@
-def calc_fact(n):
-    fact = 1
-    for i in range(1, n+1):   # loop from 1 to n
-        fact *= i
-    print(fact)
-
-calc_fact(5)
+def square(n):
+    
+        result=n*n
+        return(result)
+        
+print(square(4))
---
+++ Day5/np2.py
@@ -0,0 +1,15 @@
+#functions in math
+import math
+print(math.sqrt(16))  # 4.0
+
+print(math.pow(2, 3))  # 8.0
+
+print(math.factorial(5))  # 120
+
+print(math.ceil(4.2))  # 5
+
+print(math.floor(4.9))  # 4
+
+print(math.pi)  # 3.141592653589793
+
+
